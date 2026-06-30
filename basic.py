ad479d6a9f683d303d1ff3f301853f88556cb4ff
foysall34 <foysal.cse13@gmail.com>
2026-06-30 08:50:19 +0600

    list of array

--- basic.py
+++ basic.py
@@ -1,2 +1,37 @@
 age = 30
+age1 = 25.2
+name = "John"
+
+is_learning = False
+print("Is learning Python:", is_learning)
+print("My age is :" , age1)
 print("My age is" , age)
+print("My name is" , name)
+
+
+marks = 50
+
+if marks >= 80:
+    print("you got A+ grade")
+elif marks >= 70:
+    print("you got A grade")
+else:
+    print("you got B grade")
+
+
+# for i in range(1,21):
+#     print("Number is:", i)
+
+
+
+for i in range(50, 101):
+    print("Number i: " , i)
+
+
+fruits = ["apple", "mango", "dim"]
+print(fruits[2])
+
+
+
+for fruit in fruits:
+    print("I eat" , fruit)
