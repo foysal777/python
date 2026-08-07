9345213c0e625f050297e11bcbd14c9f5d22dd36
Foysal Hossain <118968299+foysal777@users.noreply.github.com>
2026-08-07 10:49:01 +0600

    test : yield

--- preparation/generator.py
+++ preparation/generator.py
@@ -4,4 +4,18 @@ numbers = [1, 2, 3, 4, 5]
 
 for i in numbers:
     print(i)
-    time.sleep(3)
+    # time.sleep(3)
+
+
+def demo():
+    return 100
+
+
+print(demo())
+
+
+def hello():
+    yield 13
+    print("Hello")
+
+print(next(hello()))
